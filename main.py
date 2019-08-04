86abcd6aa2cdd07bb704475e2aee15cd8353a4d4
Glenn Franxman <glenn@sixfeetup.com>
2019-08-04 16:19:13 -0400

    This version uses pin transistions to drive the state transitions, and defaults to IDLE until the high switch goes high-to-low indicating that a drain-fill-idle sequence should begin.

--- main.py
+++ main.py
@@ -22,46 +22,110 @@ fresh_pump = p0
 dirty_pump = p2
 
 
-
+IDLE = -1
 DRAINING = 0
 FILLING = 1
 
-mode = DRAINING
+mode = IDLE
+top = True
+bottom = True
 
-def tick(t):
-    global mode
+last_top = top
+last_bottom = bottom
+
+top_went_high = False
+top_went_low = False
+
+bottom_went_high = False
+bottom_went_low = False
+
+
+def read_switches():
+    global top, last_top, top_went_high, top_went_low
+    global bottom, last_bottom, bottom_went_high, bottom_went_low
+
+
     top = top_float_switch.value()
     bottom = bottom_float_switch.value()
 
-    if top:
-        if bottom:
-            fresh_pump.on()
-            dirty_pump.on()
-            mode = DRAINING  # next for next time we are between states
-        else:
-            print("error -- top is floating, but bottom is sinking")
+    if top and not last_top:
+        top_went_high = True
+        top_wwent_low = False
+    elif last_top and not top:
+        top_went_low = True
+        top_went_high = False
+    last_top = top
 
+    if bottom and not last_bottom:
+        bottom_went_high = True
+        bottom_went_low = False
+    elif last_bottom and not bottom:
+        bottom_went_low = True
+        bottom_went_high = False
+    last_bottom = bottom
+
+
+def print_mode():
+    global mode
+    if mode == IDLE:
+        print("IDLE")
+    elif mode == DRAINING:
+        print("DRAINING")
+    elif mode == FILLING:
+        print("FILLING")
     else:
-        if bottom:
-            # keep filling or draining
-            print("mode:" + str(mode))
-            if mode == DRAINING:
-                print("draining")
-                fresh_pump.on()
-                dirty_pump.off()  # keep draining
-            else:
-                print("filling")
-                fresh_pump.off()
-                dirty_pump.on()
+        print("BAD MODE:" + str(mode))
 
-        else:
-           # top and bottom have sunk.
-           # time to decide if we are filling or draining
-           mode = FILLING
-           fresh_pump.off()  # ON
-           dirty_pump.on()
+def set_mode(new_mode):
+    global mode
+    global top_went_high, top_went_low, bottom_went_high, bottom_went_low
 
+    mode = new_mode
+    if mode == IDLE:
+        print("changing to IDLE")
+        fresh_pump.on()
+        dirty_pump.on()
+    elif mode == DRAINING:
+        print("changing to DRAINING")
+        fresh_pump.on()
+        dirty_pump.off()
+    elif mode == FILLING:
+        print("changing to FILLING")
+        fresh_pump.off()  # ON
+        dirty_pump.on()
+    else:
+        print("BAD MODE:" + str(mode))
+
+    clear_transitions()
+
+
+def clear_transitions():
+    global top_went_high, top_went_low, bottom_went_high, bottom_went_low
+    top_went_high = False
+    top_went_low = False
+    bottom_went_high = False
+    bottom_went_low = False
+
+
+def tick(t):
+    global mode, top_went_high, top_went_low, bottom_went_high, bottom_went_low
+    global top, bottom
+
+    read_switches()
+
+    if top_went_low:
+        set_mode(DRAINING)
+
+    elif bottom_went_low:
+        set_mode(FILLING)
+
+    if top and mode == FILLING:
+        set_mode(IDLE)
+
+    if top and not bottom:
+        print("error -- top is floating, but bottom is sinking")
+
+    print_mode()
 
 rt.init(period=1000, mode=Timer.PERIODIC, callback=tick)
 
-    
